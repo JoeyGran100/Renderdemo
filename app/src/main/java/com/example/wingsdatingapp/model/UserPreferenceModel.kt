693ae878bba0bdd94bd16ede0fc5688d39ac9795
josef <josefgranlof@hotmail.com>
2025-01-09 18:51:17 +0100

    ultimate second new Commit

---
+++ app/src/main/java/com/example/wingsdatingapp/model/UserPreferenceModel.kt
@@ -0,0 +1,15 @@
+package com.example.wingsdatingapp.model
+
+import androidx.annotation.Keep
+import androidx.room.Entity
+import androidx.room.Index
+import kotlinx.serialization.Serializable
+
+
+// I added this 2025/01/05
+@Keep
+@Entity(tableName = "UserPreference", indices = [Index(value = ["id"], unique = true)])
+@Serializable
+data class UserPreferenceModel (
+    val preference:String?=null,
+)
